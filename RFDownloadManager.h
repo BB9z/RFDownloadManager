461202da60db3784a3d37445f6a746d8edafa7d5
BB9z <bb9z@live.com>
2012-08-29 15:06:51 +0800

    + RFDownloadManager 雏形

--- RFDownloadManager.h
+++ RFDownloadManager.h
@@ -1,29 +1,59 @@
-//
-//  RFDownloadManager.h
-//  RFDownloadManager example
-//
-//  Created by BB9z on 12-8-20.
-//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
-//
+/*!
+    RFDownloadManager
+
+    Copyright (c) 2012 BB9z
+    http://github.com/bb9z/RFKit
+
+    The MIT License (MIT)
+    http://www.opensource.org/licenses/mit-license.php
+*/
+
+/** Example
+    
+ 
+ 
+ */
 
 #import <Foundation/Foundation.h>
-#import "AFNetworking.h"
+#import "RFFileDownloadOperation.h"
+
+@class RFDownloadManager;
+
+@protocol RFDownloadManagerDelegate <NSObject>
+@optional
+// 成功下载
+- (void)RFDownloadManager:(RFDownloadManager *)downloadManager operationCompleted:(RFFileDownloadOperation *)operation;
+
+// 下载失败
+- (void)RFDownloadManager:(RFDownloadManager *)downloadManager operationFailed:(RFFileDownloadOperation *)operation;
+
+// 用于下载状态更新，进度、速度
+- (void)RFDownloadManager:(RFDownloadManager *)downloadManager operationStateUpdate:(RFFileDownloadOperation *)operation;
+
+@end
 
 @interface RFDownloadManager : NSObject
+@property (RF_WEAK, nonatomic) id<RFDownloadManagerDelegate> delegate;
 
-@property (readonly, atomic) BOOL isDownloading;
+// 未完成
+@property (readonly, nonatomic) BOOL isDownloading;
 
+// 未完成
+@property (readonly, nonatomic) uint processingOperationCount;
+
+- (RFDownloadManager *)initWithDelegate:(id<RFDownloadManagerDelegate>)delegate;
 + (RFDownloadManager *)sharedInstance;
-- (BOOL)addURL:(NSURL *)url fileStorePath:(NSString *)destinationFilePath;
-- (BOOL)startQueue;
+
+- (RFFileDownloadOperation *)addURL:(NSURL *)url fileStorePath:(NSString *)destinationFilePath;
+
+- (void)startAll;
+- (void)pauseAll;
+- (void)cancelAll;
+- (void)startOperation:(RFFileDownloadOperation *)operation;
+- (void)pauseOperation:(RFFileDownloadOperation *)operation;
+- (void)cancelOperation:(RFFileDownloadOperation *)operation;
 
 @end
 
-@interface RFFileDownloadOperation : AFURLConnectionOperation
 
-@property (copy, nonatomic) NSString *destinationFilePath;
-
-- (RFFileDownloadOperation *)initWithRequest:(NSURLRequest *)urlRequest fileStorePath:(NSString *)destinationFilePath;
-
-@end
 
--- RFDownloadManager.m
+++ RFDownloadManager.m
@@ -1,17 +1,10 @@
-//
-//  RFDownloadManager.m
-//  RFDownloadManager example
-//
-//  Created by BB9z on 12-8-20.
-//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
-//
 
 #import "RFDownloadManager.h"
 
 @interface RFDownloadManager ()
-@property (RF_STRONG, atomic) NSMutableArray *requrests;
+@property (RF_STRONG, atomic) NSMutableArray *requrests;        // 未完成
 @property (RF_STRONG, atomic) NSMutableArray *requrestOperations;
-@property (assign, readwrite, atomic) BOOL isDownloading;
+@property (assign, readwrite, nonatomic) BOOL isDownloading;
 
 @property (copy, nonatomic) NSString *tempFileStorePath;
 @end
@@ -21,8 +14,19 @@
 - (RFDownloadManager *)init {
     if (self = [super init]) {
         self.isDownloading = NO;
+        self.requrests = [NSMutableArray array];
+        self.requrestOperations = [NSMutableArray array];
+        return self;
     }
-    return self;
+    return nil;
+}
+
+- (RFDownloadManager *)initWithDelegate:(id<RFDownloadManagerDelegate>)delegate {
+    if (self = [self init]) {
+        self.delegate = delegate;
+        return self;
+    }
+    return nil;
 }
 
 + (RFDownloadManager *)sharedInstance {
@@ -34,16 +38,85 @@
 	return sharedInstance;
 }
 
-- (BOOL)addURL:(NSURL *)url fileStorePath:(NSString *)destinationFilePath {
-    if (![self.requrests containsObject:url]) {
-        [self.requrests addObject:url];
-        
-        RFFileDownloadOperation *downloadOperation = [[RFFileDownloadOperation alloc] initWithRequest:[NSURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:40] fileStorePath:destinationFilePath];
-        [self.requrestOperations addObject:downloadOperation];
-        
-        return YES;
+- (RFFileDownloadOperation *)addURL:(NSURL *)url fileStorePath:(NSString *)destinationFilePath {
+    if ([self.requrests containsObject:url]) {
+        return nil;
     }
-    return NO;
+
+    __block RFFileDownloadOperation *downloadOperation = [[RFFileDownloadOperation alloc] initWithRequest:[NSURLRequest requestWithURL:url] targetPath:destinationFilePath shouldResume:YES shouldCoverOldFile:NO];
+    
+    if (downloadOperation) {
+       
+        downloadOperation.deleteTempFileOnCancel = YES;
+        
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-retain-cycles"
+        
+        [downloadOperation setProgressiveDownloadProgressBlock:^(NSInteger bytesRead, long long totalBytesRead, long long totalBytesExpected, long long totalBytesReadForFile, long long totalBytesExpectedToReadForFile) {
+            
+            //            dout_float(totalBytesExpected)
+            //            dout_float(totalBytesReadForFile)
+            //            dout_float(totalBytesExpectedToReadForFile)
+            
+            if (self.delegate && [self.delegate respondsToSelector:@selector(RFDownloadManager:operationStateUpdate:)]) {
+                [self.delegate RFDownloadManager:self operationStateUpdate:downloadOperation];
+            }
+        }];
+        
+        [downloadOperation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+            if (self.delegate && [self.delegate respondsToSelector:@selector(RFDownloadManager:operationCompleted::)]) {
+                
+                [self.requrestOperations removeObject:operation];
+                
+                [self.delegate RFDownloadManager:self operationStateUpdate:downloadOperation];
+            }
+            
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            if (self.delegate && [self.delegate respondsToSelector:@selector(RFDownloadManager:operationFailed::)]) {
+                
+                [self.delegate RFDownloadManager:self operationStateUpdate:downloadOperation];
+            }
+        }];
+        
+#pragma clang diagnostic pop
+        
+        [self.requrestOperations addObject:downloadOperation];
+        [self.requrests addObject:url];
+    }
+    return downloadOperation;
+}
+
+- (void)startAll {
+    for (RFFileDownloadOperation *operation in self.requrestOperations) {
+        [self startOperation:operation];
+    }
+}
+- (void)pauseAll {
+    for (RFFileDownloadOperation *operation in self.requrestOperations) {
+        [self pauseOperation:operation];
+    }
+}
+- (void)cancelAll {
+    for (RFFileDownloadOperation *operation in self.requrestOperations) {
+        [self cancelOperation:operation];
+    }
+}
+
+- (void)startOperation:(RFFileDownloadOperation *)operation {
+    if ([operation isPaused]) {
+        [operation resume];
+    }
+    else {
+        [operation start];
+    }
+}
+- (void)pauseOperation:(RFFileDownloadOperation *)operation {
+    if (![operation isPaused]) {
+        [operation pause];
+    }
+}
+- (void)cancelOperation:(RFFileDownloadOperation *)operation {
+    [operation cancel];
 }
 
 - (BOOL)startQueue {
@@ -57,14 +130,3 @@
 
 @end
 
-@implementation RFFileDownloadOperation
-- (RFFileDownloadOperation *)initWithRequest:(NSURLRequest *)urlRequest fileStorePath:(NSString *)destinationFilePath {
-    self = [super initWithRequest:urlRequest];
-    if (self) {
-        self.destinationFilePath = destinationFilePath;
-        self.outputStream = [NSOutputStream outputStreamToFileAtPath:destinationFilePath append:YES];
-    }
-    return self;
-}
-
-@end
